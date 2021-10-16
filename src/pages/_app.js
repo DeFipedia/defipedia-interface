cea63bdd0c598351b3dfcaaac803cf6de22f5fe9
PradhuumnaPancholi <pnpancholi@protonmail.com>
2021-10-16 15:41:13 -0400

    fixed dep issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
+import {UseWalletProvider} from 'use-wallet'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <UseWalletProvider chainId={4}>
   <ChakraProvider>
     <Layout>
     <Component {...pageProps} />
     </Layout>
-  </ChakraProvider>)
+  </ChakraProvider>
+    </UseWalletProvider>
+  )
 }
 
 export default MyApp
